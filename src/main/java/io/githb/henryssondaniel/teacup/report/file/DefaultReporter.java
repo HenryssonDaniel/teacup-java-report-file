70862c8cb8f53f367619d05c2786c94779aa9811
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-05-06 17:55:30 -0400

    Add DefaultReporter
    Added the DefaultReporter, which is the implementation of the
    Reporter interface. This reporter save logs into the users home folder.

---
+++ src/main/java/io/githb/henryssondaniel/teacup/report/file/DefaultReporter.java
@@ -0,0 +1,243 @@
+package io.githb.henryssondaniel.teacup.report.file;
+
+import io.github.henryssondaniel.teacup.core.reporting.Reporter;
+import io.github.henryssondaniel.teacup.core.testing.Node;
+import io.github.henryssondaniel.teacup.core.testing.Result;
+import io.github.henryssondaniel.teacup.core.testing.Status;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Reporter that saves the logs into a file hierarchy. Each node gets its own folder together with a
+ * log file.
+ *
+ * @since 1.0
+ */
+public class DefaultReporter implements Reporter {
+  private static final String DIRECTORY_ERROR =
+      "The directory %s could not be created. All logs belonging to this directory will not be saved.";
+  private static final String DIRECTORY_EXISTS =
+      "The directory {0} does already exist. All logs belonging to this directory will not be saved.";
+  private static final String FILE_ERROR =
+      "The file %s could not be created. The logs will not be saved.";
+  private static final String LOG = ".log";
+  private static final Logger LOGGER = Logger.getLogger(DefaultReporter.class.getName());
+  private static final String STARTED = "Started";
+
+  private final Map<Node, Path> map = new HashMap<>(0);
+  private final Path realPath;
+
+  private int aborted;
+  private int failed;
+  private Path rootLog;
+  private Path rootPath;
+  private int skipped;
+  private int successful;
+
+  /**
+   * Constructor.
+   *
+   * @since 1.0
+   */
+  public DefaultReporter() {
+    this(
+        Path.of(
+            System.getProperty("user.home"),
+            ".teacup",
+            "logs",
+            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmssSSS"))));
+  }
+
+  DefaultReporter(Path realPath) {
+    this.realPath = realPath;
+  }
+
+  @Override
+  public void finished(Node node, Result result) {
+    LOGGER.log(Level.FINE, "Finished");
+
+    var status = result.getStatus();
+
+    if (status == Status.ABORTED) aborted++;
+    else if (status == Status.FAILED) failed++;
+    else successful++;
+
+    var path = Optional.ofNullable(map.get(node)).orElseGet(() -> rootLog);
+    if (path != null)
+      createHandler(path.toString())
+          .ifPresent(
+              handler ->
+                  log(
+                      handler,
+                      new LogRecord(
+                          Level.INFO,
+                          "Finished with status: "
+                              + status
+                              + " after "
+                              + (node.getTimeFinished() - node.getTimeStarted())
+                              + " ms."
+                              + result.getThrowable().map(Throwable::toString).orElse(""))));
+  }
+
+  @Override
+  public void initialize() {
+    if (Files.exists(realPath)) LOGGER.log(Level.SEVERE, DIRECTORY_EXISTS, realPath);
+    else
+      try {
+        rootPath = Files.createDirectories(realPath);
+        rootLog = createRootLog(realPath.resolve(LOG));
+      } catch (IOException e) {
+        LOGGER.log(Level.SEVERE, String.format(DIRECTORY_ERROR, realPath), e);
+      }
+  }
+
+  @Override
+  public void initialized(Iterable<? extends Node> nodes) {
+    LOGGER.log(Level.FINE, "Initialized");
+
+    if (rootPath != null) createDirectories(map, nodes, rootPath);
+  }
+
+  @Override
+  public void log(LogRecord logRecord, Node node) {
+    LOGGER.log(Level.FINE, "Log");
+
+    var path = Optional.ofNullable(map.get(node)).orElseGet(() -> rootLog);
+    if (path != null) createHandler(path.toString()).ifPresent(handler -> log(handler, logRecord));
+  }
+
+  @Override
+  public void skipped(Node node, String reason) {
+    LOGGER.log(Level.INFO, "Skipped");
+
+    var path = Optional.ofNullable(map.get(node)).orElseGet(() -> rootLog);
+    if (path != null) {
+      skipped++;
+
+      createHandler(path.toString())
+          .ifPresent(
+              handler -> log(handler, new LogRecord(Level.INFO, "Skipped with reason: " + reason)));
+    }
+  }
+
+  @Override
+  public void started(Node node) {
+    LOGGER.log(Level.FINE, STARTED);
+
+    var path = Optional.ofNullable(map.get(node)).orElseGet(() -> rootLog);
+    if (path != null)
+      createHandler(path.toString())
+          .ifPresent(handler -> log(handler, new LogRecord(Level.INFO, STARTED)));
+  }
+
+  @Override
+  public void terminated() {
+    LOGGER.log(Level.FINE, "Terminated");
+
+    map.clear();
+
+    if (rootLog != null)
+      createHandler(rootLog.toString())
+          .ifPresent(
+              handler ->
+                  log(
+                      handler,
+                      new LogRecord(
+                          Level.INFO,
+                          (aborted + failed + skipped + successful)
+                              + " tests executed, "
+                              + aborted
+                              + " aborted, "
+                              + skipped
+                              + " skipper, "
+                              + failed
+                              + " failed")));
+
+    aborted = 0;
+    failed = 0;
+    skipped = 0;
+    successful = 0;
+  }
+
+  private static void createDirectories(
+      Map<? super Node, ? super Path> map, Iterable<? extends Node> nodes, Path rootPath) {
+    for (var node : nodes) {
+      var name = rootPath.resolve(node.getName());
+
+      if (Files.exists(name)) LOGGER.log(Level.SEVERE, DIRECTORY_EXISTS, name);
+      else createDirectory(map, name, node, rootPath);
+    }
+  }
+
+  private static void createDirectory(
+      Map<? super Node, ? super Path> map, Path name, Node node, Path rootPath) {
+    try {
+      Files.createDirectories(name);
+
+      createLog(name.resolve(LOG), map, node, rootPath);
+    } catch (IOException e) {
+      LOGGER.log(Level.SEVERE, String.format(DIRECTORY_ERROR, name), e);
+    }
+  }
+
+  private static Optional<Handler> createHandler(String path) {
+    Handler handler = null;
+
+    try {
+      handler = new FileHandler(path, true);
+    } catch (IOException e) {
+      LOGGER.log(
+          Level.SEVERE,
+          String.format("The file %s could not be written to. The logs will not be saved.", path),
+          e);
+    }
+
+    return Optional.ofNullable(handler);
+  }
+
+  private static void createLog(
+      Path filename, Map<? super Node, ? super Path> map, Node node, Path rootPath) {
+    try {
+      map.put(node, Files.createFile(filename));
+
+      createDirectories(map, node.getNodes(), rootPath);
+    } catch (IOException e) {
+      LOGGER.log(Level.SEVERE, String.format(FILE_ERROR, filename), e);
+    }
+  }
+
+  private static Path createRootLog(Path filename) {
+    Path createdLog = null;
+
+    try {
+      createdLog = Files.createFile(filename);
+    } catch (IOException e) {
+      LOGGER.log(Level.SEVERE, String.format(FILE_ERROR, filename), e);
+    }
+
+    return createdLog;
+  }
+
+  private static void log(Handler handler, LogRecord logRecord) {
+    handler.setFormatter(new SimpleFormatter());
+
+    LOGGER.addHandler(handler);
+    LOGGER.setUseParentHandlers(false);
+    LOGGER.log(logRecord);
+
+    handler.close();
+    LOGGER.removeHandler(handler);
+  }
+}
---
+++ src/test/java/io/githb/henryssondaniel/teacup/report/file/DefaultReporterTest.java
@@ -0,0 +1,327 @@
+package io.githb.henryssondaniel.teacup.report.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import io.github.henryssondaniel.teacup.core.reporting.Reporter;
+import io.github.henryssondaniel.teacup.core.testing.Node;
+import io.github.henryssondaniel.teacup.core.testing.Result;
+import io.github.henryssondaniel.teacup.core.testing.Status;
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+import java.nio.file.spi.FileSystemProvider;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class DefaultReporterTest {
+  private static final String FINISHED = "finished";
+  private static final String INITIALIZED = "initialized";
+  private static final String LOG = ".log";
+  private static final String NAME = "name";
+  private static final String REASON = "reason";
+  private static final String SKIPPED = "skipped";
+  private static final String TEST = "test";
+
+  private final FileSystem fileSystem = mock(FileSystem.class);
+  private final FileSystemProvider fileSystemProvider = mock(FileSystemProvider.class);
+  private final LogRecord logRecord = mock(LogRecord.class);
+  private final Node node = mock(Node.class);
+  private final Path path = mock(Path.class);
+  private final Result result = mock(Result.class);
+  private final SeekableByteChannel seekableByteChannel = mock(SeekableByteChannel.class);
+
+  @BeforeEach
+  void beforeEach() {
+    when(node.getName()).thenReturn(NAME);
+  }
+
+  @Test
+  void finishedWhenAborted(@TempDir Path tempDir) {
+    when(result.getStatus()).thenReturn(Status.ABORTED);
+
+    Reporter reporter = new DefaultReporter(tempDir.resolve(FINISHED));
+    reporter.initialize();
+    reporter.finished(node, result);
+
+    verify(node).getTimeFinished();
+    verify(node).getTimeFinished();
+    verify(result).getStatus();
+    verify(result).getThrowable();
+  }
+
+  @Test
+  void finishedWhenFailed(@TempDir Path tempDir) {
+    when(result.getStatus()).thenReturn(Status.FAILED);
+
+    Reporter reporter = new DefaultReporter(tempDir.resolve(FINISHED));
+    reporter.initialize();
+    reporter.finished(node, result);
+
+    verify(node).getTimeFinished();
+    verify(node).getTimeFinished();
+    verify(result).getStatus();
+    verify(result).getThrowable();
+  }
+
+  @Test
+  void finishedWhenNoHandler(@TempDir Path tempDir) {
+    var file = tempDir.resolve("folder").toFile();
+
+    Reporter reporter = new DefaultReporter(file.toPath());
+    reporter.initialize();
+
+    var files = file.listFiles();
+    if (files != null) for (var child : files) deleteFile(child);
+    assertThat(file.delete()).isTrue();
+
+    reporter.finished(node, result);
+
+    verify(result).getStatus();
+    verify(result, times(0)).getThrowable();
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void finishedWhenNoNodeSuccessFul() {
+    new DefaultReporter().finished(node, result);
+
+    verify(result).getStatus();
+    verify(result, times(0)).getThrowable();
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void initializeWhenFileCanNotBeCreated() throws IOException {
+    when(fileSystem.provider()).thenReturn(fileSystemProvider);
+    when(path.resolve(LOG)).thenReturn(path);
+
+    try (var system = path.getFileSystem()) {
+      when(system).thenReturn(fileSystem);
+    }
+
+    var ioException = new IOException(TEST);
+    doThrow(ioException).when(fileSystemProvider).checkAccess(path);
+
+    try (var byteChannel = fileSystemProvider.newByteChannel(same(path), any(), any())) {
+      when(byteChannel).thenThrow(ioException);
+    }
+
+    new DefaultReporter(path).initialize();
+
+    verify(fileSystemProvider).checkAccess(path);
+    verify(fileSystemProvider).createDirectory(path);
+    verify(fileSystem, times(4)).provider();
+    verify(path).resolve(LOG);
+  }
+
+  @Test
+  void initializeWhenFolderCanNotBeCreated() throws IOException {
+    when(fileSystem.provider()).thenReturn(fileSystemProvider);
+    when(path.toAbsolutePath()).thenReturn(path);
+
+    try (var system = path.getFileSystem()) {
+      when(system).thenReturn(fileSystem);
+    }
+
+    var ioException = new IOException(TEST);
+    doThrow(ioException).when(fileSystemProvider).checkAccess(path);
+    doThrow(ioException).when(fileSystemProvider).createDirectory(path);
+
+    new DefaultReporter(path).initialize();
+
+    verify(fileSystemProvider).checkAccess(path);
+    verify(fileSystemProvider).createDirectory(path);
+    verify(fileSystem, times(3)).provider();
+    verify(path, times(0)).resolve(LOG);
+    verify(path).toAbsolutePath();
+  }
+
+  @Test
+  void initializeWhenFolderExists(@TempDir Path tempDir) {
+    new DefaultReporter(tempDir).initialize();
+    assertThat(tempDir.toFile().list()).isEmpty();
+  }
+
+  @Test
+  void initialized(@TempDir Path tempDir) {
+    Reporter reporter = new DefaultReporter(tempDir.resolve(INITIALIZED));
+    reporter.initialize();
+    reporter.initialized(Collections.singletonList(node));
+
+    verify(node).getName();
+  }
+
+  @Test
+  void initializedWhenFileCanNotBeCreated() throws IOException {
+    when(fileSystem.provider()).thenReturn(fileSystemProvider);
+    when(path.resolve(anyString())).thenReturn(path);
+
+    try (var system = path.getFileSystem()) {
+      when(system).thenReturn(fileSystem);
+    }
+
+    var ioException = new IOException(TEST);
+    doThrow(ioException).when(fileSystemProvider).checkAccess(path);
+
+    try (var byteChannel = fileSystemProvider.newByteChannel(same(path), any(), any())) {
+      when(byteChannel).thenReturn(seekableByteChannel).thenThrow(ioException);
+    }
+
+    Reporter reporter = new DefaultReporter(path);
+    reporter.initialize();
+    reporter.initialized(Collections.singletonList(node));
+
+    verify(fileSystemProvider, times(2)).checkAccess(path);
+    verify(fileSystemProvider, times(2)).createDirectory(path);
+    verify(fileSystem, times(8)).provider();
+    verify(path, times(2)).resolve(LOG);
+  }
+
+  @Test
+  void initializedWhenFolderAlreadyExisting(@TempDir Path tempDir) throws IOException {
+    var folder = tempDir.resolve(INITIALIZED);
+
+    Reporter reporter = new DefaultReporter(folder);
+    reporter.initialize();
+
+    assertThat(folder.resolve(NAME).toFile().createNewFile());
+    reporter.initialized(Collections.singletonList(node));
+
+    verify(node).getName();
+  }
+
+  @Test
+  void initializedWhenFolderCanNotBeCreated() throws IOException {
+    when(fileSystem.provider()).thenReturn(fileSystemProvider);
+    when(path.toAbsolutePath()).thenReturn(path);
+    when(path.resolve(anyString())).thenReturn(path);
+
+    try (var system = path.getFileSystem()) {
+      when(system).thenReturn(fileSystem);
+    }
+
+    var ioException = new IOException(TEST);
+    doNothing().doThrow(ioException).when(fileSystemProvider).createDirectory(path);
+    doThrow(ioException).when(fileSystemProvider).checkAccess(path);
+
+    try (var byteChannel = fileSystemProvider.newByteChannel(same(path), any(), any())) {
+      when(byteChannel).thenReturn(seekableByteChannel);
+    }
+
+    Reporter reporter = new DefaultReporter(path);
+    reporter.initialize();
+    reporter.initialized(Collections.singletonList(node));
+
+    verify(fileSystemProvider, times(2)).checkAccess(path);
+    verify(fileSystemProvider, times(2)).createDirectory(path);
+    verify(fileSystem, times(7)).provider();
+    verify(path).resolve(LOG);
+    verify(path).toAbsolutePath();
+  }
+
+  @Test
+  void initializedWhenNoRoot() {
+    new DefaultReporter().initialized(Collections.singletonList(node));
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void log(@TempDir Path tempDir) {
+    when(logRecord.getInstant()).thenReturn(Instant.EPOCH);
+    when(logRecord.getLevel()).thenReturn(Level.INFO);
+
+    Reporter reporter = new DefaultReporter(tempDir.resolve("log"));
+    reporter.initialize();
+    reporter.log(logRecord, node);
+
+    verify(logRecord).getInstant();
+    verify(logRecord, times(4)).getLevel();
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void logWhenNoRoot() {
+    new DefaultReporter().log(logRecord, node);
+
+    verifyZeroInteractions(logRecord);
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void skipped(@TempDir Path tempDir) {
+    Reporter reporter = new DefaultReporter(tempDir.resolve(SKIPPED));
+    reporter.initialize();
+    reporter.skipped(node, REASON);
+
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void skippedWhenNoRoot() {
+    new DefaultReporter().skipped(node, REASON);
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void started(@TempDir Path tempDir) {
+    Reporter reporter = new DefaultReporter(tempDir.resolve(SKIPPED));
+    reporter.initialize();
+    reporter.started(node);
+
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void startedWhenNoRoot() {
+    new DefaultReporter().started(node);
+    verifyZeroInteractions(node);
+  }
+
+  @Test
+  void terminated(@TempDir Path tempDir) throws IllegalAccessException, NoSuchFieldException {
+    Reporter reporter = new DefaultReporter(tempDir.resolve("terminated"));
+    reporter.initialize();
+    reporter.terminated();
+
+    assertThat(getMap(reporter)).isEmpty();
+  }
+
+  @Test
+  void terminatedWhenNoRoot() throws IllegalAccessException, NoSuchFieldException {
+    Reporter reporter = new DefaultReporter();
+    reporter.terminated();
+
+    assertThat(getMap(reporter)).isEmpty();
+  }
+
+  private static void deleteFile(File child) {
+    assertThat(child.delete()).isTrue();
+  }
+
+  @SuppressWarnings("unchecked")
+  private static Map<Node, Path> getMap(Reporter reporter)
+      throws IllegalAccessException, NoSuchFieldException {
+    var field = DefaultReporter.class.getDeclaredField("map");
+    field.setAccessible(true);
+
+    return (Map<Node, Path>) field.get(reporter);
+  }
+}
